d695748e9256d3dda8955664077dc6856f2588d9
Heather You <heather.fswd@gmail.com>
2019-02-01 15:50:36 -0500

    Move while loop into main and out of start

--- src/main/java/com/heatheryou/httpserver/Main.java
+++ src/main/java/com/heatheryou/httpserver/Main.java
@@ -4,13 +4,15 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        try (
-                ServerSocketWrapper serverSocketWrapper = new ServerSocketWrapper(5000);
-                Server server = new Server(serverSocketWrapper)
-        ) {
-            server.start();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
+        while (true) {
+            try (
+                    ServerSocketWrapper serverSocketWrapper = new ServerSocketWrapper(5000);
+                    Server server = new Server(serverSocketWrapper)
+            ) {
+                server.start();
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
         }
     }
 }
--- src/main/java/com/heatheryou/httpserver/Server.java
+++ src/main/java/com/heatheryou/httpserver/Server.java
@@ -13,34 +13,27 @@ public class Server implements AutoCloseable {
     }
 
     public void start() {
-        boolean done = false;
-        while (!done) {
-            try {
-                ISocketWrapper socketWrapper = serverSocketWrapper.accept();
-                BufferedReader requestReader = socketWrapper.getInputStreamReader();
-                String inputLine = requestReader.readLine();
-                if (inputLine == null) {
-                    done = true;
-                } else {
-                    List<String> requestLineList = new ArrayList<>();
-                    while (inputLine != null && inputLine.length() > 0) {
-                        requestLineList.add(inputLine);
-                        inputLine = requestReader.readLine();
-                    }
-                    RequestParser requestParser = new RequestParser();
-                    ResponseBuilder responseBuilder = requestParser.parse(requestLineList);
-                    String header = responseBuilder.setHeader();
-                    String body = responseBuilder.setBody();
-                    Response response = new Response(header, body);
-                    String responseLine = response.getResponseLine();
-                    PrintWriter printWriter = socketWrapper.getPrintWriter();
-                    printWriter.print(responseLine);
-                    printWriter.flush();
-                }
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-            }
-        }
+         try {
+             ISocketWrapper socketWrapper = serverSocketWrapper.accept();
+             BufferedReader requestReader = socketWrapper.getInputStreamReader();
+             String inputLine = requestReader.readLine();
+             List<String> requestLineList = new ArrayList<>();
+             while (inputLine != null && inputLine.length() > 0) {
+                 requestLineList.add(inputLine);
+                 inputLine = requestReader.readLine();
+             }
+             RequestParser requestParser = new RequestParser();
+             ResponseBuilder responseBuilder = requestParser.parse(requestLineList);
+             String header = responseBuilder.setHeader();
+             String body = responseBuilder.setBody();
+             Response response = new Response(header, body);
+             String responseLine = response.getResponseLine();
+             PrintWriter printWriter = socketWrapper.getPrintWriter();
+             printWriter.print(responseLine);
+             printWriter.flush();
+         } catch (Exception e) {
+             System.out.println(e.getMessage());
+         }
     }
 
     @Override
