5eba45b2169ee9772fca3fdfc814f5f877036035
Heather You <heather.fswd@gmail.com>
2019-02-07 16:10:13 -0500

    Add more constants for header and characters

--- src/main/java/com/heatheryou/httpserver/constants/CharacterSet.java
+++ src/main/java/com/heatheryou/httpserver/constants/CharacterSet.java
@@ -1,6 +1,7 @@
 package com.heatheryou.httpserver.constants;
 
 public class CharacterSet {
+    public static final String EMPTY = "";
     public static final String SP = " ";
     public static final String CRLF = "\r\n";
 }
