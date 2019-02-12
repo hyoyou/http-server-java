ff04b08b0317bd8af56582c9dc08d2abb9990a37
Heather You <heather.fswd@gmail.com>
2019-02-12 14:30:08 -0500

    Debug main function

--- src/main/java/com/heatheryou/httpserver/Main.java
+++ src/main/java/com/heatheryou/httpserver/Main.java
@@ -4,13 +4,15 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        System.out.println(args);
         while (true) {
             Router router = new Router();
             RequestParser parser = new RequestParser();
             ResponseBuilder builder = new ResponseBuilder();
+            int port = 5000;
 
             try (
-                    ServerSocketWrapper serverSocketWrapper = new ServerSocketWrapper(5000);
+                    ServerSocketWrapper serverSocketWrapper = new ServerSocketWrapper(port);
                     Server server = new Server(serverSocketWrapper, router, parser, builder)
             ) {
                 server.start();
