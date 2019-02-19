85ae9f567181e6bbe0aff15fa75e1ce81288a3b6
Heather You <heather.fswd@gmail.com>
2019-02-19 14:27:33 -0500

    Missing import statement

--- src/test/java/com/heatheryou/httpserver/ServerTest.java
+++ src/test/java/com/heatheryou/httpserver/ServerTest.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 
 import java.io.*;
 
-import static org.junit.Assert.assertEquals;
+//import static org.junit.Assert.assertEquals;
 
 public class ServerTest {
     private Router router;
